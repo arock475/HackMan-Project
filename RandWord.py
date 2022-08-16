6b76c8649ebd032330f1928f8712339e46a4b773
arock475 <75497722+arock475@users.noreply.github.com>
2022-08-16 13:39:33 -0400

    Add files via upload

--- RandWord.py
+++ RandWord.py
@@ -1,11 +1,10 @@
-# This is a temporary solution to give a random word to test the game until I write my api
-from random import seed
-from random import randint
+# This is a temporary solution to give a random word to test the game until I write an api
+import random
 
 
+# open the file that contains a list of words and choose a random one
 def rand_word():
     word_file = open("wordList.txt", 'r')
     word_list = word_file.readlines()
-    seed(1)
-    word = randint(0, 852)
-    return word_list[word]
+    return random.choice(word_list)
+
--- main.py
+++ main.py
@@ -1,34 +1,57 @@
 from RandWord import *
-# tomorrow i need to work on splitting things into more functions
+
+# tomorrow I need to work on splitting things into more functions
 # also need to get the letters that are guessed to be shown each time
 # also need to work on a gui
 # then an api
 # lots of things to do
+# my theme is going to be hacking a server, we are finding the password by playing hangman
+# so to do this basically want a black screen with green letters
+# global variables to simplify things
+print_incorrect = ["\n+---+\n    |\n    |\n    |\n   ===", "\n+---+\nO   |\n    |\n    |\n   ===",
+                   "\n+---+\nO   |\n|   |\n    |\n   ===", "\n+---+\nO   |\n/|  |\n    |\n   ===",
+                   "\n+---+\nO   |\n/|\ |\n    |\n   ===", "\n+---+\nO   |\n/|\ |\n/   |\n   ===",
+                   "\n+---+\nO   |\n/|\ |\n/ \ |\n   ==="]
+guessed_letters = []
+# Gets the word using the current functionality to choose a random word
+word = rand_word()
+size = len(word)
 
+
+# prints the letters that have been guessed and the blank spaces that still need to be guessed
+def print_after_guess(guessed_letters):
+    i = 0
+    correct = 0
+    for char in word:
+        if char in guessed_letters:
+            print(word[i], end=" ")
+            correct += 1
+        else:
+            print("_", end=" ")
+        i += 1
+    return correct
+
+
+# This function provides the main functionality of the game by calling other smaller functions
 def hackman():
-    word = rand_word()
-    correct = False
-    guess_word = ""
-    guess_word.ljust(len(word))
-    word_size = len(word) - 1
+    # show the number of letters in the word to allow the player to start guessing
+    for x in word:
+        print("_", end=" ")
+    guess_count = 0
     incorrect = 0
-    while not correct:
-        guess = input("Guess a letter\n")
-        count = 0
-        for x in word:
-            if x == guess:
-                count += 1
-                word_size -= 1
-        if word_size == 0:
-            print("You have completed the word and you win YAY!")
-            return
-        elif count > 0:
-            print("correct!\n%s is in the word %d times" %(guess, count))
+    letters_guessed = 0
+    while True:
+        print(print_incorrect[incorrect])
+        guessed_letters.append(input("Input a lowercase letter to guess\n"))
+        inc = print_after_guess(guessed_letters)
+        if inc > 0:
+            letters_guessed += inc
+            inc -= inc
         else:
             incorrect += 1
-            if incorrect > 6:
-                print("You Lose :(")
-                return
+        if letters_guessed == size:
+            print("YOU WIN!! :)")
+        guess_count += 1
 
 
 if __name__ == "__main__":
