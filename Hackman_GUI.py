a55a7da2e019ee7008034f36724af93cac1a714d
arock475 <75497722+arock475@users.noreply.github.com>
2022-08-21 14:32:59 -0400

    Add files via upload

--- Hackman_GUI.py
+++ Hackman_GUI.py
@@ -1,17 +1,47 @@
-import random
+from RandWord import rand_word
+import string
 from tkinter import *
-from Pillow import Image, ImageTk
+
 game_control = True
 
+# GUI initialization
+root = Tk()
+root.geometry('1000x1000')
+root.title('Hackman')
+root.config(bg='#000000')
+root.iconbitmap('../Hackman-Project-main/media/Hackman.ico')
+
+# get the objective word
+word = rand_word()
+
+
+def temp(letter):
+    temp_label = Label(root, text="clicked " + letter)
+    temp_label.grid(row=5, column=0)
+
+
 while game_control:
-    root = Tk()
-    root.geometry('1000x1000')
-    root.title('Hackman')
-    root.config(bg='#ffffff')
-    # put letters into the screen
-    letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
-               'V', 'W', 'X', 'Y', 'Z']
-    Dict = []
-    inc = 0
-    while inc < 26:
-        Dict.append(ImageTk.PhotoImage(Image.open('Buttons/' + letters[inc] + '.png')))
+    # put the current hangman image on the screen
+
+    # put the current state of the objective word on the screen
+    x = 0
+    for a in word:
+        my_label = Label(root, text="_____ ", fg="#20C20E", bg="black")
+        my_label.grid(column=x, row=0)
+        x += 1
+    # put letter buttons into the screen
+    letters = list(string.ascii_uppercase)
+    i, x, y = 0, 0, 1
+    while i < 26:
+        button = Button(root, text=letters[i], command=lambda i=i: temp(letters[i]), fg="#20C20E", bg="black", padx=30,
+                        pady=15)
+        button.grid(column=x, row=y)
+        i += 1
+        x += 1
+        if x % 8 == 0:
+            y += 1
+            x = 0
+
+    game_control = False
+
+root.mainloop()
