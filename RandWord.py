a8849d0b0ea125edfa97fb857aa365509595a173
arock475 <75497722+arock475@users.noreply.github.com>
2022-08-29 10:29:02 -0400

    Add files via upload

--- RandWord.py
+++ RandWord.py
@@ -1,10 +1,14 @@
 # This is a temporary solution to give a random word to test the game until I write an api
 import random
+import requests
+import json
 
 
 # open the file that contains a list of words and choose a random one
 def rand_word():
-    word_file = open("wordList.txt", 'r')
-    word_list = word_file.readlines()
-    return random.choice(word_list)
+    URL = 'http://arock475.pythonanywhere.com'
+    output = requests.get(URL)
 
+    json_vals = output.json()
+    word = json_vals['output']
+    return word
